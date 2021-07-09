17cebf9cfdb6cb98a5e95653bd333cda87a897c8
Martyn Eggleton <meggleton@utcsheffield.org.uk>
2021-07-09 09:26:14 +0100

    moves to argpase & latest PyDriller
    Fixes #5 with commit based to and from parameters

--- README.md
+++ README.md
@@ -6,19 +6,19 @@ Actually this will work on all Git Logs but...
 Make git logs easier for use in scenerioas when communicating the progress of a project to none experts.
 
 ## Note
-
+-
 This isn't great but it does the job (a bit).
 
 ## Installation
 
 ```bash
-pip3 install pydriller Pygments Jinja2
+python -m pip install pydriller Pygments Jinja2
 ```
 
 ## Usage
 
 ```bash
-main.py -i <repo> -o <outputfile>
+python main.py -i <repo> -o <outputfile>
 ```
 
 repo is the local repository folder
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 ## pip3 install pydriller Pygments Jinja2
 
 import sys, getopt
@@ -9,73 +8,70 @@ from pygments.lexers import PythonLexer
 from pygments.lexers import guess_lexer, guess_lexer_for_filename
 from pygments.formatters import HtmlFormatter
 from jinja2 import Template, Environment, FileSystemLoader
-from pydriller import RepositoryMining
+from pydriller import Repository
+#import imgkit
 
 
-def main(argv):
-    repo = '../mote_light_painting'
-    outputfile = 'mlp2.html'
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:",["irepo=","ofile="])
-    except getopt.GetoptError:
-        print ('main.py -i <repo> -o <outputfile>')
-        sys.exit(2)
-    
-    for opt, arg in opts:
-        if opt == '-h':
-            print ('main.py -i <repo> -o <outputfile>')
-            sys.exit()
-        elif opt in ("-i", "--irepo"):
-            repo = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
+from html2image import Html2Image
+hti = Html2Image()
 
-    #print ('Input file is "', repo)
-    #print ('Output file is "', outputfile)
 
+
+def main(args):
+    #print ("args", args)
+    kwargs = {}
+    for param in args.keys():
+        if not param in ["outputfile", "repo"]:
+            if args[param]:
+                kwargs[param] = args[param]
+
+    #print("kwargs", kwargs)
     commits = []
-    for commit in RepositoryMining(repo).traverse_commits():
+
+    for commit in Repository(args['repo'], **kwargs).traverse_commits():
+        print(commit.author.name, commit.msg)
         newcommit = {
             "author":commit.author.name,
             "msg":commit.msg,
             "date":commit.author_date,
             "modifications":[]
         }
-        for m in commit.modifications:
-            newmod = {
-                "filename":m.filename,
-                "change_type":m.change_type.name,
-                #"source_code":m.source_code
-                "hlcode": ""
-            }
+        if hasattr(commit, "modified_files"):
+            for m in commit.modified_files:
+                newmod = {
+                    "filename":m.filename,
+                    "change_type":m.change_type.name,
+                    #"source_code":m.source_code
+                    "hlcode": ""
+                }
 
-            print(
-                m.change_type.name,
-                " modified {}".format(m.filename),
-            )
-            
-
-            if m.filename.lower().endswith(('.png', '.jpg', '.jpeg', 'gif')) == False:
+                print(
+                    m.change_type.name,
+                    " modified {}".format(m.filename),
+                )
                 
-                if m.change_type.name == "ADD":
-                    lexer = guess_lexer(m.source_code)
-                    newmod["hlcode"] = highlight(m.source_code, lexer, HtmlFormatter(wrapcode=True,linenos=True))
-                        
-                if m.change_type.name == "MODIFY":
-                    lexer = guess_lexer(m.source_code)
-                    #print (m.changed_methods)
-                    lines = str.splitlines(m.source_code)
-                    
-                    for c in m.changed_methods:
-                        code = "\n".join(lines[c.__dict__['start_line']-1: c.__dict__['start_line']+ c.__dict__['nloc']])
-                        newmod["hlcode"] += "<br /><br />"+highlight(code, lexer, HtmlFormatter(wrapcode=True))#linenostart=c.__dict__['start_line'],wrapcode=True,linenos=True))
-                        #print(code)
-                        #print(newmod["hlcode"])
 
-            newcommit["modifications"].append(newmod)
+                if m.filename.lower().endswith(('.png', '.jpg', '.jpeg', 'gif')) == False:
+                    
+                    if m.change_type.name == "ADD":
+                        lexer = guess_lexer(m.source_code)
+                        newmod["hlcode"] = highlight(m.source_code, lexer, HtmlFormatter(wrapcode=True,linenos=True))
+                            
+                    if m.change_type.name == "MODIFY":
+                        lexer = guess_lexer(m.source_code)
+                        #print (m.changed_methods)
+                        lines = str.splitlines(m.source_code)
+                        
+                        for c in m.changed_methods:
+                            code = "\n".join(lines[c.__dict__['start_line']-1: c.__dict__['start_line']+ c.__dict__['nloc']])
+                            newmod["hlcode"] += "<br /><br />"+highlight(code, lexer, HtmlFormatter(wrapcode=True))#linenostart=c.__dict__['start_line'],wrapcode=True,linenos=True))
+                            #print(code)
+                            #print(newmod["hlcode"])
+
+                newcommit["modifications"].append(newmod)
         commits.append(newcommit)
 
-    of = open(outputfile, "w")
+    of = open(args['outputfile'], "w")
     file_loader = FileSystemLoader('templates')
     env = Environment(loader=file_loader)
 
@@ -89,12 +85,28 @@ def main(argv):
     of.write(output)
     of.close()
 
+    
+    #imgkit.from_file('output.html', 'output.jpg')
+    
+    hti.screenshot(
+        html_file=args['outputfile'],
+        save_as='output.png'
+    )
+
     #import pdfkit 
     #pdfkit.from_file(outputfile, outputfile+'.pdf') 
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    import argparse
+    parser = argparse.ArgumentParser(prog='Prettify GitHub Log', description='Make git logs easier for use in scenerioas when communicating the progress of a project to none experts.')
+    parser.add_argument("-i", "--repo", default='https://github.com/UTCSheffield/prettify-gh-log.git')
+    parser.add_argument("-o", "--outputfile", default="output2.html")
+    parser.add_argument("-f", "--fromcommit", dest="from_commit")
+    parser.add_argument("-t", "--tocommit", dest="to_commit")
+    args = parser.parse_args()
+    
+    main(vars(args))
    
    
 
