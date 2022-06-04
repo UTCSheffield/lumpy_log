f9140f8f6c2e6adc7f813cda384f3a3766e24fa9
Martyn Eggleton <martyn.eggleton@gmail.com>
2022-06-04 11:53:10 +0100

    Minor Tweaks

--- lumpy_log/lumpy_log.py
+++ lumpy_log/lumpy_log.py
@@ -140,20 +140,3 @@ def main(args):
                         #file1.write("\n\n".join(newcommit["markdown"]))
                         file1.write(newcommit["markdown"])
     
-                    
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(prog='Prettify GitHub Log', description='Make git logs easier for use in scenerioas when communicating the progress of a project to none experts.')
-    parser.add_argument("-r", "--repo", default='https://github.com/UTCSheffield/lumpy_log.git')
-    parser.add_argument("-o", "--outputfolder", default="output")
-    parser.add_argument("-f", "--fromcommit", dest="from_commit")
-    parser.add_argument("-t", "--tocommit", dest="to_commit")
-    parser.add_argument("-a", "--allbranches", action="store_true")
-    parser.add_argument("-v", "--verbose", action="store_true")
-    parser.add_argument("-b", "--branch", dest="branch")
-    parser.add_argument("--force", action="store_true")
-    parser.add_argument("-d", "--dryrun", action="store_true")
-    args = parser.parse_args()
-    
-    main(vars(args))
-    
