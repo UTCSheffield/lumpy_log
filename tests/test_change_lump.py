de7493771a16ce5066a9369139f7768746fa35fd
Martyn Eggleton <martyn.eggleton@gmail.com>
2022-06-04 11:54:10 +0100

    Move to pytest for Visual Studio Code integration

--- tests/test_change_lump.py
+++ tests/test_change_lump.py
@@ -1,28 +1,86 @@
-import unittest
-from nose2.tools import params
+import pytest
+
+
 
 from lumpy_log.change_lump import ChangeLump
 from lumpy_log.languages import Languages
 
 languages = Languages()
+
 JavaScript = languages.getByExtension(".js")
 JavaScript_lines = '''
+// Comment
+
+// The function returns the product of p1 and p2
+function myFunction(p1, p2) {
+  return p1 * p2;   
+}
+
+
 '''
+
 Python = languages.getByExtension(".py")
-Python_lines = '''
+Python_lines = '''from importlib_resources import files
+# Comment 
+
+class Languages(object):
+    def __init__(self, LANGUAGES_PATH = "languages.yml"):
+        self.LANGUAGES_PATH = LANGUAGES_PATH
+        sLanguages = files("lumpy_log").joinpath(self.LANGUAGES_PATH).read_text()
+        self.LANGUAGES = yaml.safe_load(sLanguages)
+
+    # TODO : Cache the fetched language objects
+
+    @property
+    def list(self):
+        return self.LANGUAGES.keys()
+
 '''
 Handlebars = languages.getByExtension(".hbs")
 Handlebars_lines = '''
+{{! Comment }}
+
+## Commit : {{msg}}
+
+By "{{ author }}" on {{ date }}
+
 '''
 
 HTML = languages.getByExtension(".html")
-HTML_lines = '''
-'''
-class TestChangeLump(unittest.TestCase):
-    @params((JavaScript, JavaScript_lines), (Python, Python_lines), (Handlebars, Handlebars_lines), (HTML, HTML_lines))
-    def test___init__start(self, lang, lines, start=None):
-        Lump = ChangeLump(lang, lines)
+HTML_lines = '''<!DOCTYPE html>
+<!-- Comment -->
+<html>
+<body>
 
+<p>This is a paragraph.</p>
+
+<!-- <p>This is another paragraph </p> -->
+
+<p>This is a paragraph too.</p>
+
+</body>
+</html>
+'''
+class TestChangeLump():
+    @pytest.mark.parametrize("lang, lines, line, isComment", [
+        (JavaScript, JavaScript_lines,1,False), 
+        (Python, Python_lines,1,False), 
+        #(Handlebars, Handlebars_lines,1,False), 
+        (HTML, HTML_lines,1,False),
+        (JavaScript, JavaScript_lines,2,True), 
+        (Python, Python_lines,2,True), 
+        #(Handlebars, Handlebars_lines,2,True), 
+        (HTML, HTML_lines,2,True)
+    ])
+    def test___init__is_comment(self, lang, lines, line, isComment):
+        Lump = ChangeLump(lang, lines, verbose=True)
+        assert Lump._lineIsComment(line) == isComment
+    
+    
+    '''def test___init__start(self, lang, lines, start=1, lump_lines=1):
+        Lump = ChangeLump(lang, lines,start=start, verbose=True)
+        self.assertEqual(len(Lump.code.splitlines()), lump_lines)
+    '''
     '''
     def test___init__lines(self, lang, lines, start=None, end=None, func=None, verbose=False):
 
--- tests/test_languages.py
+++ tests/test_languages.py
@@ -1,29 +1,36 @@
-import unittest
-from nose2.tools import params
+import pytest
 
 from lumpy_log.languages import Languages
 
 languages = Languages()
 
-class TestLanguages(unittest.TestCase):
-    @params((".py", "Python"), (".java", "Java"), (".js", "JavaScript"), (".css", "CSS"))
+class TestLanguages():
+    @pytest.mark.parametrize("ext, lang", 
+        [(".py", "Python"), (".java", "Java"), (".js", "JavaScript"), (".css", "CSS")]
+    )
     def test__getByExtension(self, ext, lang):
-        self.assertEqual(languages._getByExtension(ext), lang)
+        assert languages._getByExtension(ext) == lang
 
-    @params((".py", "Python"), (".java", "Java"), (".js", "JavaScript"), (".css", "CSS"))
+    @pytest.mark.parametrize("ext, lang", 
+        [(".py", "Python"), (".java", "Java"), (".js", "JavaScript"), (".css", "CSS")]
+    )
     def test_getByExtension(self, ext, lang):
         languages = Languages()
-        self.assertEqual(languages.getByExtension(ext).name, lang)
+        assert languages.getByExtension(ext).name == lang
 
-    @params((".py", "python"), (".java", "java"), (".js", "javascript"), (".css", "css"))
+    @pytest.mark.parametrize("ext, mdname", 
+        [(".py", "python"), (".java", "java"), (".js", "javascript"), (".css", "css")]
+    )
     def test_mdname(self, ext, mdname):
         languages = Languages()
-        self.assertEqual(languages.getByExtension(ext).mdname, mdname)
+        assert languages.getByExtension(ext).mdname == mdname
 
-    @params((".py", "Python"), (".java", "C"), (".js", "C"), (".css", "CSS"))
+    @pytest.mark.parametrize("ext, comment_family", 
+        [(".py", "Python"), (".java", "C"), (".js", "C"), (".css", "CSS")]
+    )
     def test_comment_family(self, ext, comment_family):
         languages = Languages()
-        self.assertEqual(languages.getByExtension(ext).comment_family, comment_family)
+        assert languages.getByExtension(ext).comment_family == comment_family
 
 
 
